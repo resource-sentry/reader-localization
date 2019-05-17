38fdb16696254e49646ebf9179da632bb160c821
Nicolas Siver <nicolas@siver.im>
2019-05-16 21:39:26 -0400

    add constnats with iana url

---
+++ src/model/constants.js
@@ -0,0 +1,4 @@
+module.exports = Object.freeze({
+    IANA_LANGUAGE_REGISTRY_URL: 'https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry',
+    REGISTRY_FILENAME         : 'iana_registry.txt'
+});
